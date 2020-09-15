87ef583e5468bd086e678e6759cabf06e065eb2d
MrColteR <MrColteR@mail.ru>
2020-09-15 21:35:47 +0300

    Added surname for Hello world app

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,6 +7,7 @@ namespace CourseApp
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boleslav Piguta");
             Console.ReadLine();
         }
     }
